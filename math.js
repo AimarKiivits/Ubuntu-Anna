4bc5456f098ce7aa1b586bf53d8d0169b8313ea0
Aima <Aimarkiivits1@gmail.com>
2023-09-08 13:22:38 +0300

    String + osa math

---
+++ math.js
@@ -0,0 +1,12 @@
+const num1 = 100;
+const num2 = 500;
+
+result = num1 / num2;
+result = num1 % num2;
+result = num1 * num2;
+result = num1 - num2;
+result = num1 + num2;
+
+result = Math.PI;
+
+console.log(result);
---
+++ string.js
@@ -0,0 +1,8 @@
+const first = "Aimar";
+const last = "Kiivits";
+
+let val = first + " " + last;
+
+val2 = last.slice(0,3);
+
+console.log(val2);
