06e5b3247c4167b35a5548324315d46b15400d52
Aima <Aimarkiivits1@gmail.com>
2023-09-08 12:46:06 +0300

    Test

---
+++ test.js
@@ -0,0 +1,19 @@
+let name = "Aimar Kiivits";
+console.log(name);
+const surname = "Kiivits";
+
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+numbers.push(6);
+console.log(numbers)
+
+const person = {
+    name: "Aimar",
+    lastname: "Kiivits",
+    age: 18,
+    things:{
+        yes: "Da",
+        no: "Niet",
+    },
+    };
+console.log(person.yes);
