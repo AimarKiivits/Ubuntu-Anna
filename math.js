a41ea19074ec79f7bc6807a311e4dc1eb4a94252
Aima <Aimarkiivits1@gmail.com>
2023-09-27 21:17:48 +0300

    Math is now my new test file.

--- math.js
+++ math.js
@@ -1,12 +1,1 @@
-const num1 = 100;
-const num2 = 500;
-
-result = num1 / num2;
-result = num1 % num2;
-result = num1 * num2;
-result = num1 - num2;
-result = num1 + num2;
-
-result = Math.PI;
-
-console.log(result);
+console.log(2.8 % 1)
